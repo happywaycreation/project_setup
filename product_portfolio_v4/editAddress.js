0eaa5f5e8b7d3b5d66b0aa32f5c27d59cde2e3ec
happywaycreation <happywaycreations@gmail.com>
2025-02-23 05:16:04 +0530

    Update editAddress.js

--- product_portfolio_v4/editAddress.js
+++ product_portfolio_v4/editAddress.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             }
         
             try {
-                const response = await fetch(`http://147.93.106.209:3000/fetch-address?aid=${aid}`);
+                const response = await fetch(`https://147.93.106.209:3000/fetch-address?aid=${aid}`);
                 
                 if (!response.ok) {
                     throw new Error("Failed to fetch address information.");
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 console.log('addressData:', addressData);
         
                 try {
-                    const response = await fetch(`http://147.93.106.209:3000/update-address`, {
+                    const response = await fetch(`https://147.93.106.209:3000/update-address`, {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json"
